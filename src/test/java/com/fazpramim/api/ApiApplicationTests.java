e51301dac1aac434a6a6fc6e84778619dd5c40de
allefhmedeiros <allefhmedeiros@gmail.com>
2025-06-20 00:26:30 -0300

    Implementação inicial de projeto e infraestrutura.

---
+++ src/test/java/com/fazpramim/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazpramim.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
