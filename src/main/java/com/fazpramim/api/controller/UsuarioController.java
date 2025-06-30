ca97186c913511ef91f0bbe46f57ca1487839c99
allefhmedeiros <allefhmedeiros@gmail.com>
2025-06-30 17:05:22 -0300

    Implementação do objeto Page que irá nos auxiliar na paginação, classificação e controle das requisições realizadas pelas aplicações cliente.

--- src/main/java/com/fazpramim/api/controller/UsuarioController.java
+++ src/main/java/com/fazpramim/api/controller/UsuarioController.java
@@ -7,6 +7,9 @@ import com.fazpramim.api.repository.UsuarioRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,9 +28,18 @@ public class UsuarioController {
         //System.out.println(dados);
     }
 
+    //Page vai devovler informações e funções de paginação. Abaixo o page vai devolver 3 registros por pagina e carregar a página 4 (Começando por 0,1,2,3,4,5...)
+    //http://localhost:8080/usuarios?size=3&page=4
+
+    //No exemplo abaixo nós ordenamos a resposta da requisição.
+    //http://localhost:8080/usuarios?sort=dataNascimento,desc
+
+    //@PageableDefault(size=10, sort = {"nome"}) irá configurar a devolução padrão caso cliente não defina o total de regustros e a classificação que deseja.
+    //Isso previne problemas de performance.
+
     @GetMapping
-    public List<dtoDadosListagemUsuario> listarUsuarios(){
-        return repository.findAll().stream().map(dtoDadosListagemUsuario::new).toList();
+    public Page<dtoDadosListagemUsuario> listarUsuarios(@PageableDefault(size=10, sort = {"nome"}) Pageable paginarDevolucao){
+        return repository.findAll(paginarDevolucao).map(dtoDadosListagemUsuario::new);
     }
 
 }
